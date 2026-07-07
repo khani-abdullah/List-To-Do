749c4257ce4f7359b5f8d68cdfa2119805f71fe8
khani-abdullah <khaniabdullah3434@gmail.com>
2026-07-07 14:59:22 +1200

    added js

--- js_project/index.js
+++ js_project/index.js
@@ -1,18 +1,20 @@
 
-const checkIcon = document.getElementById('icon1');
-  const taskText = document.getElementById('task1');
+for (let i = 1; i <= 3; i++) {
+  const checkIcon = document.getElementById(`icon${i}`);
+  const taskText = document.getElementById(`task${i}`);
 
   let checked = false;
 
-  checkIcon.addEventListener('click', () => {
+  checkIcon.addEventListener("click", () => {
     checked = !checked;
 
     if (checked) {
-      checkIcon.src = "videos_imgs/check_mark.png"; 
+      checkIcon.src = "../videos_imgs/check_mark.png"; 
       taskText.classList.add("line-through", "opacity-70");
-    } else {
-      checkIcon.src = "videos_imgs/check_mark.png"; 
+    } 
+    else {
+      checkIcon.src = "../videos_imgs/img 3 (repeat).png"; 
       taskText.classList.remove("line-through", "opacity-70");
     }
   });
-  
+}
