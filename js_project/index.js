e86924274a888e37c8c02d6e09ba1a3a4c497bfa
khani-abdullah <khaniabdullah3434@gmail.com>
2026-07-07 18:56:35 +1200

    completed the js

--- js_project/index.js
+++ js_project/index.js
@@ -1,20 +1,64 @@
 
-for (let i = 1; i <= 3; i++) {
-  const checkIcon = document.getElementById(`icon${i}`);
-  const taskText = document.getElementById(`task${i}`);
+  const tasks = [
+    { id: 1, text: "Go for a walking" },
+    { id: 2, text: "Do homework" },
+    { id: 3, text: "Plan the party" }
+  ];
 
-  let checked = false;
+  const taskList = document.getElementById("taskList");
+  const taskInput = document.getElementById("taskInput");
+  const addBtn = document.getElementById("addBtn");
+  function renderTask(task) {
+    const wrapper = document.createElement("div");
+    wrapper.className =
+      "bg-[#C4BABA40] flex border-[1px] max-w-[735px] w-full rounded-[85px] justify-between px-[33px] p-4 mx-auto";
 
-  checkIcon.addEventListener("click", () => {
-    checked = !checked;
+    const p = document.createElement("p");
+    p.id = `task${task.id}`;
+    p.className = "text-white text-[40px] font-normal";
+    p.textContent = task.text;
 
-    if (checked) {
-      checkIcon.src = "../videos_imgs/check_mark.png"; 
-      taskText.classList.add("line-through", "opacity-70");
-    } 
-    else {
-      checkIcon.src = "../videos_imgs/img 3 (repeat).png"; 
-      taskText.classList.remove("line-through", "opacity-70");
+    const iconContainer = document.createElement("div");
+    iconContainer.className = "flex gap-[10px] justify-center items-center";
+
+    const checkIcon = document.createElement("img");
+    checkIcon.id = `icon${task.id}`;
+    checkIcon.src = "../videos_imgs/img 3 (repeat).png";
+    checkIcon.alt = "check";
+    checkIcon.className = "max-w-[50px] w-full cursor-pointer";
+
+    const deleteIcon = document.createElement("img");
+    deleteIcon.src = "../videos_imgs/img 4 (repeat).png";
+    deleteIcon.alt = "delete";
+    deleteIcon.className = "max-w-[50px] w-full cursor-pointer";
+
+    iconContainer.appendChild(checkIcon);
+    iconContainer.appendChild(deleteIcon);
+
+    wrapper.appendChild(p);
+    wrapper.appendChild(iconContainer);
+    taskList.appendChild(wrapper);
+    let checked = false;
+    checkIcon.addEventListener("click", () => {
+      checked = !checked;
+      checkIcon.src = checked
+        ? "../videos_imgs/check_mark.png"
+        : "../videos_imgs/img 3 (repeat).png";
+      p.style.textDecoration = checked ? "line-through " : "none";
+    });
+    deleteIcon.addEventListener("click", () => {
+      taskList.removeChild(wrapper);
+    });
+  }
+
+  tasks.forEach(renderTask);
+  addBtn.addEventListener("click", () => {
+    const text = taskInput.value;
+    if (text !== "") {
+      const newTask = { id: tasks.length + 1, text };
+      tasks.push(newTask);
+      renderTask(newTask);
+      taskInput.value = ""; 
     }
   });
-}
+  console.log(addBtn);
